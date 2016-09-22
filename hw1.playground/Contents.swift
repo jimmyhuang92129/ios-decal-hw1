88b1cff926befb0f9801f9795a0773b8d49e7ebe
Jimmy Huang <jimmyhuang2017@gmail.com>
2016-09-21 22:37:48 -0700

    Finished HW1 Assignment!

--- hw1.playground/Contents.swift
+++ hw1.playground/Contents.swift
@@ -22,11 +22,11 @@ class Words {
 //: ### variables the same type? If not, why?
 
 
-//: [EXPLAIN YOUR ANSWER HERE]
+//: [EXPLAIN YOUR ANSWER HERE] The values passed into the function are of the optional class. In this situation it could either be a string or nil. Meanwhile the values set as instance variables the values set as "implicitly unwrapped options" and they must be strings.
 
 
 //: ## Q2: Variable Types and Function Types
-    func arePalindromes(_ words: [String]) -> Bool {
+    class func arePalindromes(_ words: [String]) -> Bool {
         let reversedWords = words.map() {String($0.characters.reversed())}
         let numElements = words.count
         
@@ -34,19 +34,22 @@ class Words {
             if words[i] != reversedWords[i] {
                 return false
             }
+            
         }
+        return true
     }
+    
 //: ### Why does the compiler dislike the **for loop**? Fix it.
 //: ### What else is wrong with this function? You may have to refer to (but **not**
 //: ### change) the code at the very bottom. Debug the function.
 
 
-//: [EXPLAIN YOUR ANSWER HERE]
+//: [EXPLAIN YOUR ANSWER HERE] Nothing wrong with the for loop as explained by piazza post. There is a clear distinction between a class function and an instance function in Swift. Down below in the code we are not suppose to modify, the program calls the function using the Words class rather than an instance object of the Words class. Also you need a tag in the function parameter "_ "The final item wrong with the function is that it can never return true. Looking at the code, we know that arePalindromes should return true if the words pass through the for loop. Therefore I added a "return true" right after the for loop.
 
 
 //: ## Q3: More Functions and Object Initialization
-    class func isAnagram() -> Bool {
-        var countLetters : [Character : Int] //Line X
+    func isAnagram() -> Bool {
+        var countLetters = [Character : Int]() //Line X
         var lenA = self.wordA.characters.count
         var lenB = self.wordB.characters.count
         
@@ -81,7 +84,7 @@ class Words {
             }
         }
         
-        return nil
+        return true
     }
 //: ### What is the problem with declaring **countLetters** as we do in **Line X**,
 //: ### and then using it in **Line Y**? Fix it (by only changing **Line X**).
@@ -89,7 +92,7 @@ class Words {
 //: ### change) the code at the very bottom. Debug the function.
 
 
-//: [EXPLAIN YOUR ANSWER HERE]
+//: [EXPLAIN YOUR ANSWER HERE] The syntax for declaring "countLetters" is incorrect. Another issue with this function is that its output is a boolean vluae that should be either true or false. It should not be "nil" as indicated in the last line. Finally, this function should not be a class method because in the code below, this function is only called on an instance of the class object. 
     
     
 }
